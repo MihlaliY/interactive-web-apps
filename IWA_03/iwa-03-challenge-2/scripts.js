06536c4e0d1033ad2f6f5a24f356bea37cf10703
Mihlali <mihlaliyono@gmail.com>
2023-10-19 13:05:19 +0200

    adds in iwa-challenge 1 and 2

---
+++ IWA_03/iwa-03-challenge-2/scripts.js
@@ -0,0 +1,5 @@
+import { display as displayA } from "./nwabisa.js";
+import { display as displayB } from "./johannes.js";
+import { display as displayC } from "./alex.js";
+
+console.log("Roles:", displayA, displayB, displayC);
