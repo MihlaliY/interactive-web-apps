5893c83973adba0734ad4fd23ec4597ad0eea1d8
Mihlali <mihlaliyono@gmail.com>
2023-11-07 21:32:59 +0200

    adds in correction to my IWA11 after class session

--- IWA_11/challenge-1/script.js
+++ IWA_11/challenge-1/script.js
@@ -1,20 +1,38 @@
 const root1 = document.querySelector('dl[data-key = "order1"]')
-const biscuits1 = document.querySelector("div.biscuits dd.count")
-const donuts1 = document.querySelector("div.donuts dd.count")
-const pancakes1 = document.querySelector("div.pancakes dd.count")
-const status1 = document.querySelector("div.status dd")
+const biscuits1 = document.querySelector(
+  'dl[data-key = "order1"] div.biscuits dd.count'
+)
+const donuts1 = document.querySelector(
+  'dl[data-key = "order1"] div.donuts dd.count'
+)
+const pancakes1 = document.querySelector(
+  'dl[data-key = "order1"] div.pancakes dd.count'
+)
+const status1 = document.querySelector('dl[data-key = "order1"] div.status dd')
 
 const root2 = document.querySelector('dl[data-key = "order2"]')
-const biscuits2 = document.querySelector("div.biscuits dd.count")
-const donuts2 = document.querySelector("div.donuts dd.count")
-const pancakes2 = document.querySelector("div.pancakes dd.count")
-const status2 = document.querySelector("div.status dd")
+const biscuits2 = document.querySelector(
+  'dl[data-key = "order2"] div.biscuits dd.count'
+)
+const donuts2 = document.querySelector(
+  'dl[data-key = "order2"] div.donuts dd.count'
+)
+const pancakes2 = document.querySelector(
+  'dl[data-key = "order2"] div.pancakes dd.count'
+)
+const status2 = document.querySelector('dl[data-key = "order2"] div.status dd')
 
 const root3 = document.querySelector('dl[data-key = "order3"]')
-const biscuits3 = document.querySelector("div.biscuits dd.count")
-const donuts3 = document.querySelector("div.donuts dd.count")
-const pancakes3 = document.querySelector("div.pancakes dd.count")
-const status3 = document.querySelector("div.status dd")
+const biscuits3 = document.querySelector(
+  'dl[data-key = "order3"] div.biscuits dd.count'
+)
+const donuts3 = document.querySelector(
+  'dl[data-key = "order3"] div.donuts dd.count'
+)
+const pancakes3 = document.querySelector(
+  'dl[data-key = "order3"] div.pancakes dd.count'
+)
+const status3 = document.querySelector('dl[data-key = "order3"] div.status dd')
 
 biscuits1.innerText = root1.getAttribute("data-biscuits")
 donuts1.innerText = root1.getAttribute("data-donuts")
